8be6314fc2e61ddf369e4519e59abb86d8ee8ed2
TV\Tristan <tristan.visnar@student.um.si>
2017-06-04 08:58:18 +0200

    zip it

--- src/pages/room/room.ts
+++ src/pages/room/room.ts
@@ -4,6 +4,8 @@ import { Camera } from '@ionic-native/camera';
 import { SelectorRoomPage } from '../selector-room/selector-room';
 import { Http, Jsonp } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+import { Response } from '@angular/http';
 
 /**
  * Generated class for the RoomPage page.
@@ -16,10 +18,25 @@ import 'rxjs/add/operator/map';
   selector: 'page-room',
   templateUrl: 'room.html',
 })
+
+
+export class sessionData{
+  users: Array<Object>;
+  id_teme:number;
+  theme:String;
+  session_duration: number;
+  username_selector: String;
+  room_name: String;
+  num_of_players:number;
+}
+
 export class RoomPage {
 
   public base64Image: string;
   public feedback:Object;
+  public sessionID:number;
+  public users: Array<Object>;
+  public dataItem:sessionData;
 
   public Rdata;
   public user1;
@@ -30,6 +47,7 @@ export class RoomPage {
     this.Rdata = this.navParams.get("roomdata");
     this.user1 = this.navParams.get("user1");
     this.sessInfo = this.navParams.get("sessionInfo");
+    this.start();
   }
 
   ionViewDidLoad() {
@@ -43,12 +61,45 @@ export class RoomPage {
     console.log(JSON.stringify(this.sessInfo));
   }
 
+
+
   isSet(obj){
     if(obj == null || obj == ""){
       return false;
     }
     return true;
   }
+
+  /*
+  users: Array<Object>;
+  id_teme:number;
+  theme:String;
+  session_duration: number;
+  username_selector: String;
+  room_name: String;
+  num_of_players:number;
+  */
+
+  assignSessionData(data:any){
+    this.dataItem.users = data.USERS;
+    this.dataItem.id_teme = data.ID_THEME;
+    this.dataItem.theme = data.THEME;
+    this.dataItem.session_duration = data.SESSION_DURATION;
+    this.dataItem.username_selector = data.USERNAME_SELECTOR;
+    this.dataItem.users = data.USERS;
+  }
+
+  start(){
+    this.getData().subscribe(data => this.assignSessionData(data) );
+  }
+
+  // Uses http.get() to load a single JSON file
+   getData() : Observable<any[]> {
+     return Observable.interval(1000).flatMap(() => this.http.get('http://164.8.230.124/tmp/snapcomp/api.php/rooms/sessionData/1/')
+      .map((res:Response) => res.json())
+    );
+   }
+
 
   pic()
   {
